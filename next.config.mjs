55dda09ff41a7890c8f760912e3341d3e08888a9
soaryong <soaryong.c@gmail.com>
2024-11-08 18:28:38 +0900

    image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh5.googleusercontent.com"],
+    domains: [
+      "lh5.googleusercontent.com",
+      "www.bell.ca",
+      "www.koolatron.com",
+      "encrypted-tbn0.gstatic.com",
+      "img.croket.co.kr",
+      "i0.wp.com",
+      "www.coffeebeandirect.com",
+    ],
   },
 };
 
